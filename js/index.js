e81f44f46b584d3e3821dd8492fb8e6291d60400
human-programmer-vr <dima.ostryakov@yandex.ru>
2024-06-15 00:38:23 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,12 +2,25 @@ const menuNode = document.querySelector('.nav-menu');
 const showNode = document.querySelector('#showMenu');
 const hideNode = document.querySelector('#hideMenu');
 
+const btnAutorizeNode = document.querySelector('.autorize');
+const btnRegisterNode = document.querySelector('.registration');
+
 showMenu.addEventListener(`click`, () => {
 	menuNode.classList.add('show-menu');
 	menuNode.classList.remove('hide-menu');
 });
 
 hideNode.addEventListener(`click`, () => {
+	menuNode.classList.add('hide-menu');
+	menuNode.classList.remove('show-menu');
+});
+
+btnAutorizeNode.addEventListener(`click`, () => {
+	menuNode.classList.add('hide-menu');
+	menuNode.classList.remove('show-menu');
+});
+
+btnRegisterNode.addEventListener(`click`, () => {
 	menuNode.classList.add('hide-menu');
 	menuNode.classList.remove('show-menu');
 });
